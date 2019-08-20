92dc3d524ce21fc1108689700bd7a4c2520a023d
tanakaminato213 <qwsazx012345@icloud.com>
2019-08-20 13:03:13 +0900

    マイグレーションファイルをまとめる

--- db/migrate/20190814083949_add_details_tousers.rb
+++
@@ -1,9 +0,0 @@
-class AddDetailsTousers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users,:current_sign_in_at, :datetime
-    add_column :users,:last_sign_in_at, :datetime
-    add_column :users,:current_sign_in_ip, :string
-    add_column :users,:last_sign_in_ip, :string
-    add_column :users,:sign_in_count, :integer, default: 0, null: false
-  end
-end
--- db/migrate/20190815145630_remove_author_fromusers.rb
+++ db/migrate/20190815145630_remove_author_fromusers.rb
@@ -15,3 +15,13 @@ class RemoveAuthorFromusers < ActiveRecord::Migration[5.2]
     add_column :users,:sign_in_count, :integer, default: 0, null: false
   end
 end
+
+class AddDetailsTousers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users,:current_sign_in_at, :datetime
+    add_column :users,:last_sign_in_at, :datetime
+    add_column :users,:current_sign_in_ip, :string
+    add_column :users,:last_sign_in_ip, :string
+    add_column :users,:sign_in_count, :integer, default: 0, null: false
+  end
+end
