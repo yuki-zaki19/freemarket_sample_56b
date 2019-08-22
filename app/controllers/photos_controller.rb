b7675fcc79ca17c7cff5f0fae1f9036b2cd81292
MASATO-YAKUSHIGAWA <m.yks.022@gmail.com>
2019-08-22 20:37:56 +0900

    fix sns_credential

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -12,7 +12,8 @@ class PhotosController < Devise::RegistrationsController
     @user = User.new(user_params)
     if @user.save!
       @identification = Identification.new(indif_params)
-      if @identification.save
+      @sns_credential = SnsCredential.new(uid: @user.uid, provider: @user.provider, user_id: @user.id)
+      if @identification.save == @sns_credential.save
         sign_in @user
         redirect_to products_path
       else
@@ -53,4 +54,5 @@ class PhotosController < Devise::RegistrationsController
     @indif = {}
     @indif.merge(last_name: @date["last_name2"], first_name: @date["first_name2"], last_name_kana: @date["last_name_kana2"], first_name_kana: @date["first_name_kana2"],phone_number: @date["phone_number2"], birthday: @birthday.to_s,user_id:@user.id)
   end
+
 end
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -13,19 +13,20 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def callback_for(provider)
     #押されたボタンによってproviderが変わる。
-    @user = User.find_for_oauth(request.env["omniauth.auth"])
+    @user = User.where(uid: request.env["omniauth.auth"].uid)
     #@userにUser.find_for_oauthを代入。
     #requestでfacebook,googleのアカウント情報を取得している。
     #envでomniauth.authという環境変数を管理している。
     if @user.present?
       #@userに値があれば以下の処理が行われる。
-      user = User.where(uid: uid, provider: provider, email: auth.info.email).first
-      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
+      # user = User.where(uid: uid, provider: provider, email: auth.info.email).first
+      # flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
       redirect_to :root
     else
       #なければ以下の処理が行われる。
       session["devise.#{provider}_data"] = request.env['omniauth.auth']
-      user = User.new()
+      @user = User.new()
+      @sns_credential = SnsCredential.new()
       if data = session["devise.#{provider}_data"]["extra"]["raw_info"]
         @user.nickname = data["name"] if @user.nickname.blank?
         @user.email = data["email"] if @user.email.blank?
@@ -33,7 +34,6 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
         @user.uid = data["sub"] if @user.uid.blank?
         @user.password = Devise.friendly_token[0,20] if @user.password.blank?
         render 'devise/registrations/new'
-        binding.pry
       end
     end
   end
