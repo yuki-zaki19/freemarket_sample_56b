0e3749e67dcceb0adeee9c18fe5c4faac8b3553e
tanakaminato213 <qwsazx012345@icloud.com>
2019-08-22 22:03:57 +0900

    commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :basic_auth, if: :production?
   before_action :configure_permitted_parameters, if: :devise_controller?
-  protect_from_forgery with: :exception
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
@@ -15,18 +13,6 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:city])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:address])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:building])
-  end
-
-  private
-
-  def production?
-    Rails.env.production?
-  end
-  
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    end
   end
 end
 
