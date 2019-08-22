39fd6b134e7706e1c10f8b77e158beec42e95cee
tanakaminato213 <qwsazx012345@icloud.com>
2019-08-22 20:38:08 +0900

    commit

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -17,24 +17,24 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     #@userにUser.find_for_oauthを代入。
     #requestでfacebook,googleのアカウント情報を取得している。
     #envでomniauth.authという環境変数を管理している。
+    binding.pry
     if @user.present?
       #@userに値があれば以下の処理が行われる。
-      user = User.where(uid: uid, provider: provider, email: auth.info.email).first
       flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
       redirect_to :root
     else
       #なければ以下の処理が行われる。
       session["devise.#{provider}_data"] = request.env['omniauth.auth']
-      user = User.new()
-      if data = session["devise.#{provider}_data"]["extra"]["raw_info"]
-        @user.nickname = data["name"] if @user.nickname.blank?
-        @user.email = data["email"] if @user.email.blank?
-        @user.provider = "#{provider}" if @user.provider.blank?
-        @user.uid = data["sub"] if @user.uid.blank?
-        @user.password = Devise.friendly_token[0,20] if @user.password.blank?
+      @user= User.new()
+        if data = session["devise.#{provider}_data"]["extra"]["raw_info"]
+          @user.nickname = data["name"] if @user.nickname.blank?
+          @user.email = data["email"] if @user.email.blank?
+          @user.provider = "#{provider}" if @user.provider.blank?
+          @user.uid = data["sub"] if @user.uid.blank?
+          @user.password = Devise.friendly_token[0,20] if @user.password.blank?
         render 'devise/registrations/new'
         binding.pry
-      end
+  end
     end
   end
 end
