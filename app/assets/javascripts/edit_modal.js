7b2eea505cff52a5fb5740b964d97c8c355c11fb
MASATO-YAKUSHIGAWA <m.yks.022@gmail.com>
2019-08-25 15:09:44 +0900

    create edit_modal

---
+++ app/assets/javascripts/edit_modal.js
@@ -0,0 +1,29 @@
+$(function(){  // 「.modal_open」をクリックしたらモーダルと黒い背景を表示する 
+  $(document).on('click', '.prev-image-content__image-box__prev__update--edit', function(){ 
+    $('body').append('<div class="delete_modal_bg"></div>');
+    $('.delete_modal_bg').fadeIn(); 
+
+    $("body").css({overflow:'hidden'}); //背景固定
+      function modalResize(){
+        var w = $(window).width();
+        var h = $(window).height();
+        var x = (w - $(".edit_modal").outerWidth(true)) / 2;
+        var y = (h - $(".edit_modal").outerHeight(true)) / 2;
+        $(".edit_modal").css({'left': x + 'px','top': "10" + 'px'});
+      }
+      modalResize(); //真ん中表示
+    $(".edit_modal").fadeIn(); // modalをフェードインで表示
+    $('.edit_modal__inner__btn--cancel').off().click(function(){ // .modal_bgか.modal_closeをクリックしたらモーダルと背景をフェードアウトさせる
+      $('.edit_modal').fadeOut();
+      $('.delete_modal_bg').fadeOut('slow',function(){
+        $('.delete_modal_bg').remove();
+        $('html, body').removeClass('lock');
+        $("body").css({overflow:''}); //背景戻す
+      });
+    });
+ // ウィンドウがリサイズされたらモーダルの位置を再計算する
+  $(window).on('resize', function(){
+    modalResize();
+  });
+});
+});
